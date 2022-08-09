dc1155d5610d267e8c4106146fced89caf8182db
nick3point5 <kenny.trinh4444@gmail.com>
2022-08-09 14:53:19 -0500

    png added

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as carWhite } from './car_white.png';
+export { default as carRed } from './car_red.png';
+export { default as carGreen } from './car_green.png';
+export { default as carBlue } from './car_blue.png';
--- src/modules/classes/Car.ts
+++ src/modules/classes/Car.ts
@@ -6,6 +6,7 @@ import {
 	createBoxPolygon,
 	polyIntersect,
 } from '@/modules'
+import { carWhite, carBlue, carRed, carGreen } from '@/assets'
 
 export class Car {
 	x: number
@@ -25,7 +26,9 @@ export class Car {
 	brain: NeuralNetwork | undefined
 	best: boolean
 	render: boolean
+	color: string
 	score: number
+	image: HTMLImageElement
 
 	constructor(
 		x = 0,
@@ -52,18 +55,47 @@ export class Car {
 		this.render = render
 		this.score = -Infinity
 		this.polygon = createBoxPolygon(this)
+		this.color = this.getColor()
+		this.image = new Image()
+		this.image.src = this.getImage()
 
 		this.controls = new Controls(type, this)
 	}
 
 	private assessDamage(roadBorders: pointType[][], traffic: Car[]) {
 		for (let i = 0; i < roadBorders.length; i++) {
-			if (polyIntersect(this.polygon, roadBorders[i])) return true
+			if (polyIntersect(this.polygon, roadBorders[i])) {
+				this.color = this.getColor()
+				return true
+			}
 		}
 		for (let i = 0; i < traffic.length; i++) {
-			if (polyIntersect(this.polygon, traffic[i].polygon)) return true
+			if (polyIntersect(this.polygon, traffic[i].polygon)) {
+				this.color = this.getColor()
+				return true
+			}
 		}
 		return false
+	}
+
+	private getImage(): string {
+		if (this.isDamaged) {
+			return carWhite
+		}
+		if (this.type === 'ai') {
+			if (this.best) {
+				console.log("best")
+				return carGreen
+			}
+			return carBlue
+		}
+		if (this.type === 'traffic') {
+			return carRed
+		}
+		if (this.type === 'user') {
+			return carGreen
+		}
+		return ''
 	}
 
 	private getColor(): string {
@@ -71,7 +103,9 @@ export class Car {
 			return 'gray'
 		}
 		if (this.type === 'ai') {
-			if (this.best) return 'rgba(0,255,255,1)'
+			if (this.best) {
+				return 'rgba(0,255,255,1)'
+			} 
 			return 'rgba(0,0,255,0.5)'
 		}
 		if (this.type === 'traffic') {
@@ -84,8 +118,24 @@ export class Car {
 	}
 
 	draw(ctx: CanvasRenderingContext2D) {
-		if(!this.render) return
-		ctx.fillStyle = this.getColor()
+		if (!this.render) return
+		if (this.best && this.sensor) {
+			this.sensor.draw(ctx)
+		}
+		// ctx.save()
+		// ctx.translate(this.x, this.y)
+		// ctx.rotate(-this.angle)
+		// ctx.drawImage(
+		// 	this.image,
+		// 	-this.width / 2,
+		// 	-this.height / 2,
+		// 	this.width,
+		// 	this.height
+		// )
+		// ctx.restore()
+
+		this.color = this.getColor()
+		ctx.fillStyle = this.color
 		ctx.beginPath()
 		ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
 		for (let i = 0; i < this.polygon.length; i++) {
@@ -93,9 +143,6 @@ export class Car {
 		}
 
 		ctx.fill()
-		if (this.best && this.sensor) {
-			this.sensor.draw(ctx)
-		}
 	}
 
 	update(
